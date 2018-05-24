2e3893a359afaae9838ecd9c9aa8abaff37c738e
Monica Tamaru Jones <mntamaru@gmail.com>
2018-05-24 20:13:20 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,3 @@
 class FiguresController < ApplicationController
-
+  
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,13 @@
 class LandmarksController < ApplicationController
+ set :views, Proc.new { File.join(root, "../views/landmarks") }
 
+ get '/' do
+
+   erb :index
+ end
+
+ get '/landmarks/new' do
+   
+   erb :new
+ end
 end
