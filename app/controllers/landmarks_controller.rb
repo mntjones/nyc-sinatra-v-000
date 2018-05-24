3d21b03aa199dddc8fc4a682aa2f6ed8eeeaf5cd
Monica Tamaru Jones <mntamaru@gmail.com>
2018-05-24 22:48:58 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 class LandmarksController < ApplicationController
  set :views, Proc.new { File.join(root, "../views/landmarks") }
 
- get '/' do
-
+ get '/landmarks' do
+  @landmarks = Landmark.all
    erb :index
  end
 
@@ -10,4 +12,22 @@ class LandmarksController < ApplicationController
    
    erb :new
  end
+ 
+ post '/landmarks' do
+   @landmark = Landmark.create(params["landmark"])
+   title = Title.find_or_create_by(name: params["title"]["name"])
+   @landmark.title = title
+   @landmark.figure_ids = params["figures"]
+   @landmark.save
+   
+   erb :show
+ end
+ 
+ get "/landmarks/:id" do
+   binding.pry
+  @landmark = Landmark.find_by_id(:id)
+  
+  erb :show
+ end
+ 
 end
