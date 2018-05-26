d119b8c073343498f6535ecdffb349678643edef
Monica Tamaru Jones <mntamaru@gmail.com>
2018-05-26 15:31:29 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,30 +7,38 @@ class FiguresController < ApplicationController
      @figures = Figure.all
      erb :index
    end
-  
+
    get '/figures/new' do
      erb :new
    end
-   
+
    post '/figures' do
      @figure = Figure.create(params["figure"])
      @figure.title_ids = params["titles"]
+     if !params["title"]["name"].empty?
+       @figure.titles << Title.create(name: params["title"]["name"])
+     end
+
      @figure.landmark_ids = params["landmarks"]
+     if !params["landmark"]["name"].empty?
+       @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+     end
+
      @figure.save
-     
+
      erb :show
    end
-   
+
    get "/figures/:id" do
     @figure = Figure.find_by_id(params[:id])
     erb :show
    end
-   
+
    get '/figures/:id/edit' do
      @figure = Figure.find_by_id(params[:id])
      erb :edit
    end
-   
+
    post '/figures/:id' do
      @figure = Landmark.find_by_id(params[:id])
      @figure.update(params["figure"])
