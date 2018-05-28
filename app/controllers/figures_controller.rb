5f22e56735ef2e8b35a3eb5541dcc032378994a5
Monica Tamaru Jones <mntamaru@gmail.com>
2018-05-28 19:59:51 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,21 +9,39 @@ class FiguresController < ApplicationController
    end
 
    get '/figures/new' do
+     @titles = Title.all
+     @landmarks = Landmark.all
      erb :new
    end
 
    post '/figures' do
-     @figure = Figure.create(params["figure"])
-     @figure.title_ids = params["titles"]
+     @figure = Figure.create(name: params["figure"]["name"])
+
+     @title_ids = params["figure"]["title_ids"]
+
      if !params["title"]["name"].empty?
        @figure.titles << Title.create(name: params["title"]["name"])
      end
 
-     @figure.landmark_ids = params["landmarks"]
+     if @title_ids
+       @title_ids.each do |id|
+         title = Title.find_by_id(id)
+         @figure.titles << title
+       end
+     end
+
+     @landmark_ids = params["figure"]["landmark_ids"]
+
      if !params["landmark"]["name"].empty?
        @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
      end
 
+     if @landmark_ids
+       @landmark_ids.each do |id|
+         landmark = Landmark.find_by_id(id)
+         @figure.landmarks << landmark
+       end
+     end
      @figure.save
 
      erb :show
@@ -40,10 +58,33 @@ class FiguresController < ApplicationController
    end
 
    post '/figures/:id' do
-     @figure = Landmark.find_by_id(params[:id])
-     @figure.update(params["figure"])
-     @figure.title_ids = params["titles"]
-     @figure.landmark_ids = params["landmarks"]
+     @figure = Figure.find_by_id(params[:id])
+     @figure.update(name: params["figure"]["name"])
+
+     @title_ids = params["figure"]["title_ids"]
+     if !params["title"]["name"].empty?
+       @figure.titles << Title.create(name: params["title"]["name"])
+     end
+
+     if @title_ids
+       @title_ids.each do |id|
+         title = Title.find_by_id(id)
+         @figure.titles << title
+       end
+     end
+
+     @landmark_ids = params["figure"]["landmark_ids"]
+     if !params["landmark"]["name"].empty?
+       @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+     end
+
+     if @landmark_ids
+       @landmark_ids.each do |id|
+         landmark = Landmark.find_by_id(id)
+         @figure.landmarks << landmark
+       end
+     end
+
      @figure.save
      erb :show
     end
